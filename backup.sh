452daab13427fd90337f4495419c32ad13f5cb00
EC2 Default User <ec2-user@ip-172-31-0-21.ap-south-1.compute.internal>
2022-08-03 07:24:14 +0000

    first commit

---
+++ backup.sh
@@ -0,0 +1,2 @@
+mkdir -p /home/ec2-user/CRQ123/backup/config/
+cp /home/ec2-user/config/cicd.config /home/ec2-user/CRQ123/backup/config/
---
+++ deploy.sh
@@ -0,0 +1,1 @@
+cp /home/ec2-user/cicd.config /home/ec2-user/config/
